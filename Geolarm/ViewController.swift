45e615c39680dc7dff0c76f2c54c77b4e90f3209
Joshua Wagner <tlrwgnr@Joshuas-MacBook-Air.local>
2017-01-24 01:40:45 -0500

    update search bar, now populates with real nearby locations, implemented actual geospatial alarm, plays alert and displays popup when less than 1000 meters away from selected location, need to enable multiple locations without restarting app and need to make alarm loop until pop up is dismissed

--- Geolarm/ViewController.swift
+++ Geolarm/ViewController.swift
@@ -12,6 +12,10 @@ import CoreLocation
 import AVFoundation
 import AudioToolbox
 
+protocol HandleMapSearch {
+    func dropPinZoom(placemark:MKPlacemark)
+}
+
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
@@ -19,8 +23,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBAction func playSoundTest(_ sender: AnyObject) {
         AudioServicesPlaySystemSound(SystemSoundID(1304))
     }
+    let alertController = UIAlertController(title: "We're Here!", message: "You have arrived at your destination", preferredStyle: UIAlertControllerStyle.alert)
+    
+//    let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+//    {
+//        locationManager.stopUpdatingLocation()
+//        (result : UIAlertAction) -> Void in
+//        print("You pressed OK")
+//    }
+    
+    func playAlarm(){
+        AudioServicesPlaySystemSound(SystemSoundID(1304))
+    }
     
     var resultSearchController: UISearchController? = nil
+    var selectedPin: MKPlacemark? = nil
+    
     
     
     var locationManager = CLLocationManager()
@@ -54,6 +72,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.distanceFilter = kCLDistanceFilterNone
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+        {
+            
+            (result : UIAlertAction) -> Void in
+            print("You pressed OK")
+        }
+        alertController.addAction(okAction)
+        
+//        locationManager.stopUpdatingLocation()
+        
+        locationSearchTable.mapView = mapView
+        locationSearchTable.handleMapSearchDelegate = self
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -64,9 +94,40 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         centerMapOnLocation(newLocation)
         
         print(long, lat)
+        if selectedPin != nil{
+            print("reached location!!!")
+//            print(selectedPin?.coordinate)
+//            print(newLocation.coordinate)
+            let pinLocation = CLLocation(latitude: (selectedPin?.coordinate.latitude)!, longitude: (selectedPin?.coordinate.longitude)!)
+            if newLocation.distance(from: pinLocation) < 1000{
+                locationManager.stopUpdatingLocation()
+                playAlarm()
+                self.present(alertController, animated: true, completion: nil)
+                mapView.removeAnnotations(mapView.annotations)
+            }
+//            locationManager.startUpdatingLocation()
+        }
     }
     func locationManager(_ manager: CLLocationManager,
                          didFailWithError error: Error){
         print(error)
+    }
+}
+
+extension ViewController: HandleMapSearch{
+    func dropPinZoom(placemark: MKPlacemark) {
+        //cache pin
+        selectedPin = placemark
+        mapView.removeAnnotations(mapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.title
+        if let city = placemark.locality, let state = placemark.administrativeArea{
+            annotation.subtitle = "(city) (state)"
+        }
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        mapView.setRegion(region, animated: true)
     }
 }
